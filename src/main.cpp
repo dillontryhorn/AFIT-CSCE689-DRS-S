98c7e49e04fdd46e46812d463631fd679e9c6554
Dillon Tryhorn <dillontryhorn@gmail.com>
2020-03-05 13:16:45 -0500

    Updated main function

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,19 @@
 #include "DivFinderSP.h"
 #include <iostream>
 
+void displayHelp(const char* execname)
+{
+    std::cout << "Proper usage: " << execname << " <large number> <verbosity>" << std::endl;
+    exit(1);
+}
+
 int main(int argc, char* argv[])
 {
-    if(argc < 2)
-    {
-        std::cout << "Proper usage: ./divfinder <large number>" << std::endl;
-        exit(1);
-    }
-    DivFinderSP my_div_finder_sp(atoll(argv[1]));
-    my_div_finder_sp.setVerbose(3);
+    if(argc < 3 || argc > 3)
+        displayHelp(argv[0]);
+
+    DivFinderSP my_div_finder_sp((LARGEINT)argv[1]);
+    my_div_finder_sp.setVerbose(atoi(argv[2]));
     my_div_finder_sp.PolRho();
     return 0;
 }
