fb72cd0c63a4ce60377efa6f19c836edef67b964
Dillon Tryhorn <dillontryhorn@gmail.com>
2020-03-05 15:11:59 -0500

    Updated main function

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <chrono>
 #include <unistd.h>
+#include <getopt.h>
 
 void displayHelp(const char* execname)
 {
@@ -20,53 +21,53 @@ int main(int argc, char* argv[])
 
     bool use_singlethread = false;
     int verbosity_in, n_threads, c = 0;
-    unsigned int num_threads = 1, verbosity = 0;
-    while ((c = getopt(argc, argv, "v:t:s")) != -1) 
-    {
-        switch (c) 
-        {
-            // Set the verbosity	    
-            case 'v':
-                verbosity_in = strtol(optarg, NULL, 10);
-                if ((verbosity_in < 0) || (verbosity_in > 3)) {
-                    std::cout << "Invalid max range. Value must be between 0 and 3" << std::endl;
-                    std::cout << "Format: " << argv[0] << " [<verbosity>] [<max_threads>]" << std::endl;
-                    exit(0);
-                }
-                verbosity = (unsigned int) verbosity_in;
-                break;
+    unsigned int num_threads = 4, verbosity = 0;
+    // while ((c = getopt(argc, argv, "v:t:s")) != -1) 
+    // {
+    //     switch (c) 
+    //     {
+    //         // Set the verbosity	    
+    //         case 'v':
+    //             verbosity_in = strtol(optarg, NULL, 10);
+    //             if ((verbosity_in < 0) || (verbosity_in > 3)) {
+    //                 std::cout << "Invalid max range. Value must be between 0 and 3" << std::endl;
+    //                 std::cout << "Format: " << argv[0] << " [<verbosity>] [<max_threads>]" << std::endl;
+    //                 exit(0);
+    //             }
+    //             verbosity = (unsigned int) verbosity_in;
+    //             break;
 
-            // Set the max number of threads to use	 
-            case 't':
-                n_threads = strtol(optarg, NULL, 10);
-                if ((n_threads < 1) || (n_threads > 10000)) {
-                    std::cout << "Invalid max threads. Value must be between 1 and 10000" << std::endl;
-                    std::cout << "Format: " << argv[0] << " [<verbosity>] [<max_threads>]" << std::endl;
-                    exit(0);
-                }
-                num_threads = (unsigned int) n_threads;
-                break;
+    //         // Set the max number of threads to use	 
+    //         case 't':
+    //             n_threads = strtol(optarg, NULL, 10);
+    //             if ((n_threads < 1) || (n_threads > 10000)) {
+    //                 std::cout << "Invalid max threads. Value must be between 1 and 10000" << std::endl;
+    //                 std::cout << "Format: " << argv[0] << " [<verbosity>] [<max_threads>]" << std::endl;
+    //                 exit(0);
+    //             }
+    //             num_threads = (unsigned int) n_threads;
+    //             break;
 
-            // Run the singlethreaded version of this code
-            case 's':
-                use_singlethread = true;
-	            break;
+    //         // Run the singlethreaded version of this code
+    //         case 's':
+    //             use_singlethread = true;
+	//             break;
 
-            case '?':
-            displayHelp(argv[0]);
-            break;
+    //         case '?':
+    //         displayHelp(argv[0]);
+    //         break;
 
-            default:
-            std::cout << "Unknown command line option '" << c << std::endl;
-            displayHelp(argv[0]);
-            break;
-        }
-    }
+    //         default:
+    //         std::cout << "Unknown command line option '" << c << std::endl;
+    //         displayHelp(argv[0]);
+    //         break;
+    //     }
+    // }
 
     if(use_singlethread)
     {
         DivFinderSP my_div_finder_sp((LARGEINT)argv[1]);
-        my_div_finder_sp.setVerbose(atoi(argv[2]));
+        my_div_finder_sp.setVerbose(verbosity);
 
         auto start = std::chrono::system_clock::now();
         my_div_finder_sp.PolRho();
@@ -75,7 +76,13 @@ int main(int argc, char* argv[])
     }
     else
     {
-        /* code */
+        DivFinderT my_div_finder_t((LARGEINT)argv[1], num_threads);
+        my_div_finder_t.setVerbose(verbosity);
+
+        auto start = std::chrono::system_clock::now();
+        my_div_finder_t.PolRho();
+        std::chrono::duration<double, std::milli> elapsed = std::chrono::system_clock::now() - start;
+        std::cout << "Time to complete: " << elapsed.count() << " ms" << std::endl;
     }
     
     
