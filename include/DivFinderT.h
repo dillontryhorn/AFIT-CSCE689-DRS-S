767a5f3a0b0512818a0e1df44b3626af98a1cd23
Dillon Tryhorn <dillontryhorn@gmail.com>
2020-03-05 14:26:53 -0500

    Added DivFinderT and main.cpp and makefile

---
+++ include/DivFinderT.h
@@ -0,0 +1,45 @@
+#ifndef DIVFINDERSP_H
+#define DIVFINDERSP_H
+
+#include <string>
+#include "DivFinder.h"
+#include <boost/multiprecision/cpp_int.hpp>
+
+// If factor has been called on a number x times, check to see if the number is prime
+const unsigned int primecheck_depth = 10;
+
+/******************************************************************************************
+ * DivFinderSP - Used as a recursive calculator for prime numbers using Pollards Rho algorithm.
+ *            A simple, recursive single process/thread version.
+ *
+ *  	   DivFinder(Const): 
+ *  	   ~DivFinder(Dest):
+ *
+ *         PolRho - function that executes the stochastic Pollards' Rho algorithm for finding
+ *                  divisors
+ *         ifPrimeBF - uses brute force using 6k+-1 optimization to determine if a number is prime
+ *
+ *  	   Exceptions: sub-classes should throw a std::exception with the what string field
+ *  	               populated for any issues.
+ *
+ *****************************************************************************************/
+
+class DivFinderT : public DivFinder { 
+   public:
+      DivFinderT(LARGEINT input_value, unsigned int threads);
+      virtual ~DivFinderT();
+
+      virtual void PolRho() override;
+
+      bool isPrimeBF(LARGEINT n, LARGEINT &divisor);
+
+   protected:
+
+      void factor(LARGEINT n);
+
+
+   private:
+        unsigned int _num_threads = 1;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "DivFinder.h"
 #include "DivFinderSP.h"
+#include "DivFinderT.h"
 #include <iostream>
+#include <chrono>
 
 void displayHelp(const char* execname)
 {
@@ -15,6 +17,11 @@ int main(int argc, char* argv[])
 
     DivFinderSP my_div_finder_sp((LARGEINT)argv[1]);
     my_div_finder_sp.setVerbose(atoi(argv[2]));
+
+    auto start = std::chrono::system_clock::now();
     my_div_finder_sp.PolRho();
+    std::chrono::duration<double, std::milli> elapsed = std::chrono::system_clock::now() - start;
+    std::cout << "Time to complete: " << elapsed.count() << " ms" << std::endl;
+    
     return 0;
 }
