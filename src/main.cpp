a53eb180706f11b2a357b64d6261884a0a85af97
Dillon Tryhorn <dillontryhorn@gmail.com>
2020-03-16 12:28:52 -0400

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,41 @@
 #include "DivFinder.h"
 #include "DivFinderSP.h"
 #include "DivFinderT.h"
-#include <iostream>
+
 #include <chrono>
-#include <unistd.h>
-#include <getopt.h>
+#include <iostream>
+#include <string>
 
 void displayHelp(const char* execname)
 {
-    std::cout << "Proper usage: " << execname << " <large number> " << std::endl;
-    std::cout << "   v: verbosity level (0-3)\n";
-    std::cout << "   t: maximum number of threads to use\n";
-    std::cout << " Try: 3840973478624489797261566375423382807\n";
+    std::cout << "Proper usage: " << execname << " <large number> <verbosity (0-3)> <number of threads (1-10000)> <run single-threaded (true/false)>" << std::endl;
+    std::cout << "Example: " << execname << " 10000 3 4 false" << std::endl;
+    std::cout << "Maximum Number: 3840973478624489797261566375423382807" << std::endl;
     exit(0);
 }
 
 int main(int argc, char* argv[])
 {
-    if(argc < 2)
+    if(argc < 4)
         displayHelp(argv[0]);
 
-    bool use_singlethread = false;
-    int verbosity_in, n_threads, c = 0;
-    unsigned int num_threads = 4, verbosity = 0;
-    // while ((c = getopt(argc, argv, "v:t:s")) != -1) 
-    // {
-    //     switch (c) 
-    //     {
-    //         // Set the verbosity	    
-    //         case 'v':
-    //             verbosity_in = strtol(optarg, NULL, 10);
-    //             if ((verbosity_in < 0) || (verbosity_in > 3)) {
-    //                 std::cout << "Invalid max range. Value must be between 0 and 3" << std::endl;
-    //                 std::cout << "Format: " << argv[0] << " [<verbosity>] [<max_threads>]" << std::endl;
-    //                 exit(0);
-    //             }
-    //             verbosity = (unsigned int) verbosity_in;
-    //             break;
+    LARGEINT large_number = static_cast<LARGEINT>(argv[1]);
 
-    //         // Set the max number of threads to use	 
-    //         case 't':
-    //             n_threads = strtol(optarg, NULL, 10);
-    //             if ((n_threads < 1) || (n_threads > 10000)) {
-    //                 std::cout << "Invalid max threads. Value must be between 1 and 10000" << std::endl;
-    //                 std::cout << "Format: " << argv[0] << " [<verbosity>] [<max_threads>]" << std::endl;
-    //                 exit(0);
-    //             }
-    //             num_threads = (unsigned int) n_threads;
-    //             break;
-
-    //         // Run the singlethreaded version of this code
-    //         case 's':
-    //             use_singlethread = true;
-	//             break;
-
-    //         case '?':
-    //         displayHelp(argv[0]);
-    //         break;
-
-    //         default:
-    //         std::cout << "Unknown command line option '" << c << std::endl;
-    //         displayHelp(argv[0]);
-    //         break;
-    //     }
-    // }
+    int verbosity = atoi(argv[2]);
+    if(verbosity < 0 || verbosity > 3) {
+        std::cout << "Verbosity must be between 0-3" << std::endl;
+        exit(0);
+    }
+    unsigned int num_threads = static_cast<unsigned int>(atoi(argv[3]));
+        if(num_threads < 1 || num_threads > 10000) {
+        std::cout << "Number of threads must be between 1-10000" << std::endl;
+        exit(0);
+    }
+    bool use_singlethread = static_cast<bool>(argv[4]);
 
     if(use_singlethread)
     {
-        DivFinderSP my_div_finder_sp((LARGEINT)argv[1]);
+        DivFinderSP my_div_finder_sp(large_number);
         my_div_finder_sp.setVerbose(verbosity);
 
         auto start = std::chrono::system_clock::now();
@@ -77,7 +45,7 @@ int main(int argc, char* argv[])
     }
     else
     {
-        DivFinderT my_div_finder_t((LARGEINT)argv[1], num_threads);
+        DivFinderT my_div_finder_t(large_number, num_threads);
         my_div_finder_t.setVerbose(verbosity);
 
         auto start = std::chrono::system_clock::now();
